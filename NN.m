8762ef38e05024ded2eed7eacf20762f093b5e42
Deandre Rucker <djrucker@mines.edu>
2025-03-10 17:37:55 -0600

    added some comments that let you know how to go through and generate the graphs, and also how to run the program, overall you should be good to just run and generate graphs and then paste that NN block into the simulation just replace the one that is already there as it is technically a different NN than the one you would generate from running the program -Deandre

--- NN.m
+++ NN.m
@@ -1,3 +1,8 @@
+%% to get figurese on training performance run this file, and then to get graphs from the NNET copy the NNET block
+%% that gets created and copy it directly into the project.slx file and run that to get the results from the simulation
+%% I already have some of the results graphs saved from running the simulation in the new folder that was added.
+
+load('training_data_cell.mat')
 net = narxnet(1:2,1:2, 10); 
 % net = configure(net, uanglecell, yanglecell); 
 disp(net.outputs{end}.size); 
@@ -21,16 +26,24 @@ perf = mse(net, Ts, Y);
 figure
 plotresponse(Ts, Y); 
 hold off; 
-E = gsubtract(Ts, Y); 
+E = gsubtract(Ts, Y);
+figure
 ploterrcorr(E);
+hold off;
+figure
 plotinerrcorr(Xs,E);
+hold off; 
 net2 = closeloop(net); 
 view(net2); 
 [Xs, Xi, Ai, Ts] = preparets(net2, uanglecell, {}, yanglecell); 
 Y = net2(Xs, Xi, Ai); 
+figure
 plotresponse(Ts,Y); 
+hold off; 
 net3 = removedelay(net); 
 view(net3); 
 [Xs,Xi, Ai, Ts] = preparets(net3, uanglecell, {}, yanglecell); 
 Y = net3(Xs, Xi, Ai); 
+figure
 plotresponse(Ts,Y); 
+hold off; 
